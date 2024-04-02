12de19c61f525be6c9d498d8e277b190eccebe3b
Brunn0B <brito.brunnob@gmail.com>
2024-04-02 19:26:30 -0300

    123

---
+++ Scroller.js
@@ -0,0 +1,16 @@
+const scrollers = document.querySelectorAll(".scroller");
+  addAnimation();
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
